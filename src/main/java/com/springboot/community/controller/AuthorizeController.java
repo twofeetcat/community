2f6707fa0eb2431aee149a03ae27b8082560c3ab
twofeetcat <1625012165@qq.com>
2020-02-26 20:50:29 +0800

    database

--- src/main/java/com/springboot/community/controller/AuthorizeController.java
+++ src/main/java/com/springboot/community/controller/AuthorizeController.java
@@ -1,27 +1,52 @@
 package com.springboot.community.controller;
 
 import com.springboot.community.dto.AccessTokenDto;
+import com.springboot.community.dto.GithubUser;
 import com.springboot.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
     @Autowired //把spring容器里面已经实例化好的实例加载到当前使用的上下文
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
+        //session是通过request得到的
         AccessTokenDto accessTokenDto = new AccessTokenDto();
-        accessTokenDto.setClient_id("d5c54e7e70dc5d5646a6");
-        accessTokenDto.setClient_secret("83d3f0ced7fb44a52c44315323e67db9565834ed");
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
         accessTokenDto.setCode(code);
-        accessTokenDto.setRedirect_uri("https://github.com/login/oauth/access_token");
+        accessTokenDto.setRedirect_uri(redirectUri);
         accessTokenDto.setState(state);
-        githubProvider.getAccessToken(accessTokenDto);
-        return "index";
+        String accessToken = githubProvider.getAccessToken(accessTokenDto);
+        GithubUser user = githubProvider.getUser(accessToken);
+        if (user != null){
+            //登录成功，写cookie和session
+            //这样就把user对象放进了session里面，这个时候相当于我们银行账户已经创建成功了，但我们没有给前端一个银行卡
+            request.getSession().setAttribute("user", user);
+            //不加前缀的话，只会把页面渲染到index，但是用户信息会出现在地址上，加上后，相当于重定向到index页面，地址也会转回index
+            return "redirect:/";
+        }else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
--- src/main/java/com/springboot/community/provider/GithubProvider.java
+++ src/main/java/com/springboot/community/provider/GithubProvider.java
@@ -1,33 +1,47 @@
 package com.springboot.community.provider;
 
+import com.alibaba.fastjson.JSON;
 import com.springboot.community.dto.AccessTokenDto;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import com.springboot.community.dto.GithubUser;
+import okhttp3.*;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.IOException;
 
 @Component //把路径仅仅上传到spring的上下文
 public class GithubProvider {
     public String getAccessToken(AccessTokenDto accessTokenDto){
-        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
 
         OkHttpClient client = new OkHttpClient();
 
-        RequestBody body = RequestBody.create(JSON, json);
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDto));
         Request request = new Request.Builder()
-                .url("https://github.com/login/oauth/access_token")
+                .url("https://github.com/login/oauth/access_token?client_id="+accessTokenDto.getClient_id()+"&client_secret="+accessTokenDto.getClient_secret()+"&code="+accessTokenDto.getCode()+"&redirect_uri="+accessTokenDto.getRedirect_uri()+"&state="+accessTokenDto.getState())
                 .post(body)
                 .build();
         try (Response response = client.newCall(request).execute()) {
             String string = response.body().string();
-            System.out.println(string);
-            return string;
-        } catch (IOException e) {
+            String tooken = string.split("&")[0].split("=")[1]; //先取到前半部分的access_token，再用等号划分取到后面的token码
+            return tooken;
+        } catch (Exception e) {
             e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+
+        try  {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);//直接将string转换成java类项目
+            return githubUser;
+        } catch (IOException e) {
         }
         return null;
     }
